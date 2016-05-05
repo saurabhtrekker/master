02628f53aee5f3398956948b874fd49c69c92b97
saurabhtrekker <saurabh.trekker@gmail.com>
2016-05-05 10:53:32 -0700

    Update iptables-ipsec-nat.sh

--- iptables-ipsec-nat.sh
+++ iptables-ipsec-nat.sh
@@ -25,7 +25,6 @@ sudo iptables -t nat -A POSTROUTING -j MASQUERADE
 # This will work only on Ubuntu
 sudo iptables-save -c > /etc/iptables.rules
 printf "#!/bin/sh \niptables-restore < /etc/iptables.rules\nexit 0\n" > /etc/network/if-pre-up.d/iptablesload
-printf "#!/bin/sh\niptables-save -c > /etc/iptables.rules\nif [ -f /etc/iptables.downrules ]; then\n   iptables-restore < /etc/iptables.downrules\nfi\nexit 0\n" > /etc/network/if-post-dow
-n.d/iptablessave
+printf "#!/bin/sh\niptables-save -c > /etc/iptables.rules\nif [ -f /etc/iptables.downrules ]; then\n   iptables-restore < /etc/iptables.downrules\nfi\nexit 0\n" > /etc/network/if-post-down.d/iptablessave
 sudo chmod +x /etc/network/if-post-down.d/iptablessave
 sudo chmod +x /etc/network/if-pre-up.d/iptablesload
