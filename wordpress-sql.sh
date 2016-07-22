7b535dd61185e91129d6f68b1fbe7de49c67e60d
saurabhtrekker <saurabh.trekker@gmail.com>
2016-07-22 11:53:01 -0700

    Update wordpress-sql.sh

--- wordpress-sql.sh
+++ wordpress-sql.sh
@@ -2,6 +2,7 @@
 
 ipaddr=`ifconfig eth0 | grep inet | awk -F " " '{print $2}' | awk -F ":" '{print $2}' | head -1`
 echo "IP Address of the machine is $ipaddr"
+sudo echo "Arguments to script are: ($1) and ($2) and PrivateIP Address: $ipaddr" >> /home/$1/log.txt
 echo "Starting..." 
 #sudo apt-get update && sudo apt-get -y upgrade
 #sudo apt-get update
