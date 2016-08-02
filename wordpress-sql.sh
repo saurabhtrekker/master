ffcd78f31323094b8ff908c5c60b48018024c99f
saurabhtrekker <saurabh.trekker@gmail.com>
2016-08-02 16:23:40 -0700

    Update wordpress-sql.sh

--- wordpress-sql.sh
+++ wordpress-sql.sh
@@ -8,11 +8,11 @@ sudo echo "Arguments to script are: ($1) and ($2) and PrivateIP Address: $ipaddr
 echo "Starting..."  >> /home/$1/log.txt
 sleep 600
 #sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo apt-get install -y wordpress
-sudo ln -s /usr/share/wordpress /var/www/html/wordpress
-sudo gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
-sudo bash /usr/share/doc/wordpress/examples/setup-mysql -n demo $ipadrr
-sudo mv /etc/wordpress/config-$ipaddr.php /etc/wordpress/config-$publicip.php
+#sudo apt-get update
+#sudo apt-get install -y apache2
+#sudo apt-get install -y wordpress
+#sudo ln -s /usr/share/wordpress /var/www/html/wordpress
+#sudo gzip -d /usr/share/doc/wordpress/examples/setup-mysql.gz
+#sudo bash /usr/share/doc/wordpress/examples/setup-mysql -n demo $ipadrr
+#sudo mv /etc/wordpress/config-$ipaddr.php /etc/wordpress/config-$publicip.php
 echo "Done..." >> /home/$1/log.txt
