b13730e1f19dcfe33946e2373621dba64e35e983
saurabhtrekker <saurabh.trekker@gmail.com>
2016-08-02 16:45:05 -0700

    Update wordpress-db.sh

--- wordpress-db.sh
+++ wordpress-db.sh
@@ -4,6 +4,7 @@
 publicip=$2
 ipaddr=`ifconfig eth0 | grep inet | awk -F " " '{print $2}' | awk -F ":" '{print $2}' | head -1`
 echo "IP Address of the machine is $ipaddr"
+echo "Sleeping for 10 minutes" >> /home/$1/log.txt
 sleep 600
 #sudo apt-get update
 #sudo apt-get install -y mysql-server
