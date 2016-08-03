75f91301d5e3c269c06636e38671951db9e792dd
saurabhtrekker <saurabh.trekker@gmail.com>
2016-08-03 11:24:59 -0700

    Create fw.sh

---
+++ fw.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#
+#CustomScript extension
+
+import os
+import os.path
+
+#key="LUFRPT03MjBDNHB3TG1rTzBDd0NBUzRqMnZzVXRsNlU9TmVvakI5MFFWM0tRNHdqTWFQaG03UT09"
+
+f = open('/home/samisra/log.txt','w')
+f.write('hi there\n') 
+f.write('python extensions for azure\n') 
+f.close() 
